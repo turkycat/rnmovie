25fb0da84b1f23f5e1a4db7bd12f7ab9defdb874
Jesse Frush <jesse.frush@gmail.com>
2020-07-09 21:26:54 -0700

    completed BFS-style search of subdirectories and all files. Script now works with one or more directory or file arguments and will complete the entire subtree

--- rnmovie.py
+++ rnmovie.py
@@ -9,7 +9,8 @@ delete_extension_list = [
   ".png",
   ".bmp",
   ".exe",
-  ".txt"
+  ".txt",
+  ".dat"
 ]
 
 regex_pattern = re.compile(r"(.*)(\d{4}).*(1080p|720p).*")
@@ -30,65 +31,69 @@ paths_to_process.pop(0)
 additional_paths = []
 files = []
 
-# iterate over the undetermined items to 
+# iterate over the undetermined items (BFS)
 while True:
   for path in paths_to_process:
     print("Path:", path)
     if not os.path.isabs(path):
-      path = os.path.join(working_directory, path)
-      print("Joined path:", path)
+      path = os.path.abspath(path)
+      print("Abs path:", path)
 
     if os.path.isdir(path):
-      additional_paths.extend(os.listdir(path))
       print(path, "is a directory")
+      for dir_item in os.listdir(path):
+        additional_paths.append(os.path.join(path, dir_item))  
     elif os.path.isfile(path):
       files.append(path)
       print(path, "is a file")
     print("-----")
   
+  #continue to traverse any newly-discovered directories
   if len(additional_paths) < 1:
     break
   else:
     paths_to_process = additional_paths
     additional_paths = []
 
-# print("files:", len(files))
+print("files found:", len(files))
+print("-----")
 
 
-# current_path = os.path.realpath(os.curdir)
-# filenames = os.listdir(current_path)
+for file_path in files:
+  print("Processing:", file_path)
+  os.chmod(file_path, 0o0777)
+  original_path, extension = os.path.splitext(file_path)
+  parent_path, original_name = os.path.split(original_path)
 
-# print("-----")
-# for filename in filenames:
-#   print("Processing:", filename)
-#   original_name, extension = os.path.splitext(filename)
-#   original_path = os.path.join(current_path, filename)
-#   os.chmod(original_path, 0o0777)
+  print("Extension:", extension)
+  extension = extension.lower()
+  if extension in delete_extension_list:
+    print("deleting file with unwanted extension:", extension)
+    os.remove(file_path)
+    continue
 
-#   extension = extension.lower()
-#   if extension in delete_extension_list:
-#     print("deleting file with unwanted extension:", extension)
-#     #os.remove(original_path)
-#     continue
+  regex_match = re.match(regex_pattern, original_name.replace(".", " "))
+  if regex_match is None:
+    print("unable to match regex pattern to file. Skipping.")
+    continue
 
-#   results = re.match(regex_pattern, original_name.replace(".", " "))
-#   title = results.group(1)
-#   year = results.group(2)
-#   quality = results.group(3)
-#   print("Title:\t", title)
-#   print("Year:\t", year)
-#   print("Quality:", quality)
+  title = regex_match.group(1)
+  year = regex_match.group(2)
+  quality = regex_match.group(3)
+  print("Title:\t  ", title)
+  print("Year:\t  ", year)
+  print("Quality:", quality)
 
-#   if extension.lower == "srt":
-#     print("Subtitle file detected. Prepending \'.English\' to the file extension")
-#     extension = ".English" + extension
+  if extension == ".srt":
+    print("Subtitle file detected. Prepending \'.English\' to the file extension")
+    extension = ".English" + extension
 
-#   new_filename = title + "[" + year + "]" + " " + quality + extension
-#   new_path = os.path.join(current_path, new_filename)
+  new_filename = title + "[" + year + "]" + " " + quality + extension
+  new_path = os.path.join(parent_path, new_filename)
 
-#   print("Renaming: ", original_path, "\nTo:", new_path)
-#   #os.rename(original_path, new_path)
+  print("Renaming: ", file_path, "\nTo:", new_path)
+  os.rename(file_path, new_path)
   
-#   print("-----")
+  print("-----")
 
 wait_on_keypress()
